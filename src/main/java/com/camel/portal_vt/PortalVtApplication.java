5b167b8c88f9e1d3bff4c1d3499442c4c93560ae
Carlos Souza <carlos.alberto.g.s@icloud.com>
2024-11-10 07:42:30 -0300

    Project base

---
+++ src/main/java/com/camel/portal_vt/PortalVtApplication.java
@@ -0,0 +1,13 @@
+package com.camel.portal_vt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalVtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalVtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camel/portal_vt/PortalVtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.portal_vt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalVtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
