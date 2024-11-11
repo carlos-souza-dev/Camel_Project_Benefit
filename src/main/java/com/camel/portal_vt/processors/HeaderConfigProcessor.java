3397eef3024d0f037b67437b6249cc9b0c789204
Carlos Souza <carlos.alberto.g.s@icloud.com>
2024-11-11 19:20:53 -0300

    Processor to config headers

---
+++ src/main/java/com/camel/portal_vt/processors/HeaderConfigProcessor.java
@@ -0,0 +1,26 @@
+package com.camel.portal_vt.processors;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.component.http.HttpMethods;
+
+public class HeaderConfigProcessor implements Processor {
+
+    private final HttpMethods httpMethod;
+
+    public HeaderConfigProcessor(HttpMethods httpMethod){
+        this.httpMethod = httpMethod;
+    }
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+        exchange.getIn().setHeader("Content-Type", "application/json");
+        exchange.getIn().setHeader("Accept", "application/json");
+        exchange.getIn().setHeader(Exchange.HTTP_METHOD, this.httpMethod);
+        exchange.getIn().removeHeader(Exchange.HTTP_PATH);
+        exchange.getIn().setHeader(Exchange.HTTP_QUERY, "bridgeEndpoint=true");
+        exchange.getIn().setHeader(Exchange.HTTP_URI, "http://localhost:5000/java-portal-vt/api/user");
+        System.out.println("OK");
+    }
+}
