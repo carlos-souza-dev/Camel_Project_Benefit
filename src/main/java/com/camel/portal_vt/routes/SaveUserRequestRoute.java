547ac6aed67763fc46254b5a9c1e94ac385e9606
Carlos Souza <carlos.alberto.g.s@icloud.com>
2024-11-25 07:20:14 -0300

    Update save user route

--- src/main/java/com/camel/portal_vt/routes/SaveUserRequestRoute.java
+++ src/main/java/com/camel/portal_vt/routes/SaveUserRequestRoute.java
@@ -3,7 +3,7 @@ package com.camel.portal_vt.routes;
 import com.camel.portal_vt.dtos.UserDTO;
 import com.camel.portal_vt.processors.HeaderConfigJavaProcessor;
 import com.camel.portal_vt.processors.ResponseSaveUserProcessor;
-import com.camel.portal_vt.processors.SaveUserProcessor;
+import com.camel.portal_vt.processors.RequestSaveUserProcessor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http.HttpMethods;
 import org.apache.camel.model.dataformat.JsonLibrary;
@@ -16,9 +16,9 @@ public class SaveUserRequestRoute extends RouteBuilder {
     public void configure() throws Exception {
         from("direct:saveUserRoute")
                 .routeId("Route - Save User")
-                .process(new SaveUserProcessor())
-                .log("Send to rest Api java-portal-vt/api")
+                .process(new RequestSaveUserProcessor())
                 .marshal().json()
+                .log("Send to rest Api java-portal-vt/api")
                 .process(new HeaderConfigJavaProcessor(HttpMethods.POST))
                 .to("http://localhost:5000/java-portal-vt/api/user?bridgeEndpoint=true")
                 .unmarshal().json(JsonLibrary.Jackson, UserDTO.class)
