13b69a2821733c467bcd486897839566272e3daf
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-01-06 07:08:42 -0300

    Route 'transportsInfoRoute' to get routing

--- src/main/java/com/camel/portal_vt/routes/TransportsInfoRoute.java
+++ src/main/java/com/camel/portal_vt/routes/TransportsInfoRoute.java
@@ -1,5 +1,6 @@
 package com.camel.portal_vt.routes;
 
+import com.camel.portal_vt.dtos.AddressesDTO;
 import com.camel.portal_vt.dtos.google.RouteInformation;
 import com.camel.portal_vt.processors.*;
 import org.apache.camel.builder.RouteBuilder;
@@ -8,7 +9,6 @@ import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import static com.camel.portal_vt.routes.AddressesRouteTest.GET_ADDRESSES_ROUTE_TEST;
 import static com.camel.portal_vt.routes.GetAddressesRoute.GET_ADDRESSES_ROUTE;
 
 @Component
@@ -25,9 +25,9 @@ public class TransportsInfoRoute extends RouteBuilder {
     public void configure() throws Exception {
         from("direct:transportsInfoRoute")
             .routeId("Route - Route information")
-            .to("direct:"+GET_ADDRESSES_ROUTE)
             .process(new HeaderConfigAddressesProcessor())
-            .process(new HeaderConfigGoogleApiProcessor(HttpMethods.GET, "${header.homeAddress}", "${header.workAddress}", this.env))
+            .marshal().json(JsonLibrary.Jackson, AddressesDTO.class)
+            .process(new HeaderConfigGoogleApiProcessor(HttpMethods.GET, "work", this.env))
             .doTry()
                 .log("Request to rest maps.googleapis.com/maps/api/directions")
                 .to("https://maps.googleapis.com/maps/api/directions/json")
@@ -35,10 +35,12 @@ public class TransportsInfoRoute extends RouteBuilder {
                 .process(new ResponseDepartureRouteInformationProcessor())
                 .choice()
                     .when(header("departureRoute"))
-                        .process(new HeaderConfigGoogleApiProcessor(HttpMethods.GET, "${header.workAddress}", "${header.homeAddress}", this.env))
-                            .to("https://maps.googleapis.com/maps/api/directions/json")
-                            .unmarshal().json(JsonLibrary.Jackson, RouteInformation.class)
-                            .process(new ResponseArrivalRouteInformationProcessor())
+                        .process(new HeaderConfigGoogleApiProcessor(HttpMethods.GET, "home", this.env))
+                        .to("https://maps.googleapis.com/maps/api/directions/json")
+                        .unmarshal().json(JsonLibrary.Jackson, RouteInformation.class)
+                        .process(new ResponseArrivalRouteInformationProcessor())
+                    .otherwise()
+                         .process(new GoogleApiErrorProcessor())
                     .endChoice()
                 .endDoTry()
             .doCatch(Exception.class)
