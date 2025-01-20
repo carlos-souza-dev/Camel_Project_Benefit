7452f57fb66acacd24e1910ec75553393de2065d
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-01-20 05:57:59 -0300

    Updated vehicle class to set velue

--- src/main/java/com/camel/portal_vt/dtos/google/Vehicle.java
+++ src/main/java/com/camel/portal_vt/dtos/google/Vehicle.java
@@ -1,9 +1,21 @@
 package com.camel.portal_vt.dtos.google;
 
+import com.camel.portal_vt.enums.Transports;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Vehicle(
         String name,
-        String type
-) {}
+        String type,
+        double value
+) {
+    public Vehicle {
+        double tariff = switch (type.toUpperCase()) {
+            case "BUS" -> 8.5;
+            case "SUBWAY" -> 5.2;
+            default -> 0.0;
+        };
+        
+        value = tariff;
+    }
+}
