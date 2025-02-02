8c76cb1036c026706bb8946767d0ceca7d766d87
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-02-02 07:36:57 -0300

    Added param route total value on SummaryRouteDTO

--- src/main/java/com/camel/portal_vt/dtos/SummaryRouteDTO.java
+++ src/main/java/com/camel/portal_vt/dtos/SummaryRouteDTO.java
@@ -2,19 +2,23 @@ package com.camel.portal_vt.dtos;
 
 import com.camel.portal_vt.dtos.google.TransitDetails;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public record SummaryRouteDTO(
         String distance,
         String duration,
-        List<TransitDetails> transitDetails
+        List<TransitDetails> transitDetails,
+        String totalValue
 ) {
     public SummaryRouteDTO(String distance,
                            String duration,
-                           List<TransitDetails> transitDetails) {
+                           List<TransitDetails> transitDetails,
+                           String totalValue) {
         this.distance = distance;
         this.duration = duration;
         this.transitDetails = transitDetails;
+        this.totalValue = totalValue;
     }
 
     ;
