4448c8f4272a97f5f07ab97fe61cd9a5d96805ff
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-02-02 06:25:35 -0300

    Updated param type and adjusted tariff value

--- src/main/java/com/camel/portal_vt/dtos/google/Vehicle.java
+++ src/main/java/com/camel/portal_vt/dtos/google/Vehicle.java
@@ -3,19 +3,23 @@ package com.camel.portal_vt.dtos.google;
 import com.camel.portal_vt.enums.Transports;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.math.BigDecimal;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Vehicle(
         String name,
         String type,
-        double value
+        BigDecimal value
 ) {
     public Vehicle {
-        double tariff = switch (type.toUpperCase()) {
-            case "BUS" -> 8.5;
-            case "SUBWAY" -> 5.2;
-            default -> 0.0;
+
+        value = switch (type.toUpperCase()) {
+            case "BUS" -> BigDecimal.valueOf(5.0);
+            case "SUBWAY" -> BigDecimal.valueOf(5.2);
+            case "TROLLEYBUS" -> BigDecimal.valueOf(6.05);
+            case "TRAIN" -> BigDecimal.valueOf(5.2);
+            case "TRAN" -> BigDecimal.valueOf(5.2);
+            default -> BigDecimal.valueOf(0.0);
         };
-        
-        value = tariff;
     }
 }
