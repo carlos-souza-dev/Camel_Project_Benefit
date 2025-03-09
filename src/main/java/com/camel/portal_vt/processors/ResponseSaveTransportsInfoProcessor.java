31f642e9dbfc79e63e82816927c9790b63359d35
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-03-09 07:01:07 -0300

    Added formatting to the full address

--- src/main/java/com/camel/portal_vt/processors/ResponseSaveTransportsInfoProcessor.java
+++ src/main/java/com/camel/portal_vt/processors/ResponseSaveTransportsInfoProcessor.java
@@ -1,5 +1,7 @@
 package com.camel.portal_vt.processors;
 
+import com.camel.portal_vt.dtos.AddressDTO;
+import com.camel.portal_vt.dtos.AddressesDTO;
 import com.camel.portal_vt.dtos.ReturnStatusDTO;
 import com.camel.portal_vt.dtos.UserDTO;
 import org.apache.camel.Exchange;
@@ -11,6 +13,10 @@ public class ResponseSaveTransportsInfoProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         UserDTO userDTO = exchange.getIn().getBody(UserDTO.class);
+        AddressDTO homeAddress = userDTO.getAddresses().homeAddress().formatAddress();
+        AddressDTO workAddress = userDTO.getAddresses().workAddress().formatAddress();
+        AddressesDTO addressesDTO = new AddressesDTO(homeAddress,workAddress);
+        userDTO.setAddresses(addressesDTO);
 
         exchange.getIn().setBody(userDTO);
     }
