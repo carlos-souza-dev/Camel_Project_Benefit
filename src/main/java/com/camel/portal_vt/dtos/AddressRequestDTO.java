0572dc874aa11441ecc9aaeaefbee502f2ef0727
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-04-13 07:28:16 -0300

    Updated record AddressRequestDTO

--- src/main/java/com/camel/portal_vt/dtos/AddressRequestDTO.java
+++ src/main/java/com/camel/portal_vt/dtos/AddressRequestDTO.java
@@ -1,7 +1,9 @@
 package com.camel.portal_vt.dtos;
 
+import com.camel.portal_vt.enums.AddressType;
+
 public record AddressRequestDTO(
         String userName,
-        String addressType,
+        AddressType addressType,
         AddressDetailsDTO addressDetails
 ) {}
