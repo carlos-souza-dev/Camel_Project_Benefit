a4d356e02d349ad5f685dac9d77e9c89af439e9b
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-04-24 00:35:02 -0300

    Created processor to get response processor history

---
+++ src/main/java/com/camel/portal_vt/processors/ResponseSaveProcessingHistProcessor.java
@@ -0,0 +1,15 @@
+package com.camel.portal_vt.processors;
+
+import com.camel.portal_vt.dtos.ProcessingHistDTO;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class ResponseSaveProcessingHistProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        ProcessingHistDTO processingHistDTO = exchange.getIn().getBody(ProcessingHistDTO.class);
+
+        exchange.getIn().setBody(processingHistDTO);
+    }
+}
