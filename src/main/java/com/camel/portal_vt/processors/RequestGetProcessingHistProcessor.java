0fbc5c07cc8e248fad332cff706601310a836514
Carlos Souza <carlos.alberto.g.s@icloud.com>
2025-04-29 01:53:25 -0300

    Updated parameter name

--- src/main/java/com/camel/portal_vt/processors/RequestGetProcessingHistProcessor.java
+++ src/main/java/com/camel/portal_vt/processors/RequestGetProcessingHistProcessor.java
@@ -10,14 +10,14 @@ public class RequestGetProcessingHistProcessor implements Processor {
         String userName = exchange.getIn().getHeader("userName", String.class);
         Integer page = exchange.getIn().getHeader("page", Integer.class);
         Integer size = exchange.getIn().getHeader("number", Integer.class);
-        String sortFirts = exchange.getIn().getHeader("sortFirts", String.class);
+        String sortFirst = exchange.getIn().getHeader("sortFirst", String.class);
         String sortLast = exchange.getIn().getHeader("sortLast", String.class);
         boolean asc = exchange.getIn().getHeader("asc", Boolean.class);
 
         exchange.setProperty("userName", userName);
         exchange.setProperty("page", page);
         exchange.setProperty("size", size);
-        exchange.setProperty("sortFirts", sortFirts);
+        exchange.setProperty("sortFirst", sortFirst);
         exchange.setProperty("sortLast", sortLast);
         exchange.setProperty("asc", asc);
     }
